0e6ff184ad519d59507bbaca49da5fdd987a5a33
Lucas Florio <lucasefe@gmail.com>
2018-11-26 17:38:14 -0500

    Detect when requires are not aligned.

---
+++ tests/lib/rules/align-requires_test.js
@@ -0,0 +1,50 @@
+/**
+ * @fileoverview align-assignment
+ * @author Broadly
+ */
+
+'use strict';
+
+const util = require('util');
+util.inspect.defaultOptions.depth = 10;
+
+// ------------------------------------------------------------------------------
+// Requirements
+// ------------------------------------------------------------------------------
+
+const rule = require('../../../lib').rules['align-requires'];
+
+
+const RuleTester = require('eslint').RuleTester;
+
+// ------------------------------------------------------------------------------
+// Tests
+// ------------------------------------------------------------------------------
+
+const ruleTester = new RuleTester({ parserOptions: { ecmaVersion: 2017 } });
+
+
+ruleTester.run('align-requires', rule, {
+  valid: [
+    {
+      code: code([
+        'const ABC = require()',
+        'const A   = require()'
+      ])
+    }
+  ],
+  invalid: [
+    {
+      code: code([
+        "const A = require('a')",
+        "const ABC = require('abc')"
+      ]),
+      errors: [{ message: 'This group of requires is not aligned' }]
+    }
+  ]
+});
+
+
+function code(lines) {
+  return lines.join('\n');
+}
